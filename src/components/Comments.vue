e74b901baa0ac8c452fb48bdf9200b784cba9cac
Gabriel Nunes <prismaticnetwork12@gmail.com>
2020-11-12 21:33:11 -0300

    creating a new component author and message

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -3,21 +3,17 @@
     <div class="">
       <h1> Comments </h1>
       <hr>
-      <p> 
-        <input type="text" v-model="author" placeholder="author" />
-      </p> 
-      <br/>
-      <p> 
-        <input type="text" v-model="message" placeholder="message"/>
-      </p>
-      <br/>
-      <button type="submit" @click="addComment">Comment</button>
-      <div v-for="(itemParaSerRenderizado, indexRender) in allComments" :key="indexRender">
-        <span>Author: <strong> {{itemParaSerRenderizado.author}}</strong></span>
-        <br/>
-        <span>Message: <strong> {{itemParaSerRenderizado.message}}</strong></span>
-        <div>
-          <a href="#" @click.prevent="removeComment(indexRender)"> DELETE </a>
+      <FormTodo @add-todo="addComment"> </FormTodo>
+      <br>
+      <div class="list-group">
+        <p v-if="comments.length <= 0"> No comments... </p>
+        <div v-else class="list-group-item" v-for="(itemParaSerRenderizado, indexRender) in allComments" :key="indexRender">
+          <span>Author: <strong> {{itemParaSerRenderizado.author}}</strong></span>
+          <br/>
+          <span>Message: <strong> {{itemParaSerRenderizado.message}}</strong></span>
+          <div>
+            <a href="#" @click.prevent="removeComment(indexRender)"> DELETE </a>
+          </div>
         </div>
       </div>
     </div>
@@ -25,27 +21,19 @@
 </template>
 
 <script>
+import FormTodo from './FormTodo'
 export default {
-  name: 'Comment',
-  
+  components: {
+    FormTodo
+  },
   data() {
     return {
-      comments: [],
-      author: '',
-      message: ''
+      comments: []
     }
   },
   methods: {
-    addComment() {
-      if(this.message.trim() === ''){
-        return;
-      }
-      this.comments.push({
-        author: this.author,
-        message: this.message
-      });
-      this.author = '',
-      this.message = ''
+    addComment(comment) {
+      this.comments.push(comment);
     },
     removeComment(index) {
       this.comments.splice(index, 1);
@@ -55,27 +43,9 @@ export default {
     allComments(){
       return this.comments.map(comment => ({
         ...comment,
-        name: comment.author.trim() === '' ? 'Annonymous' : comment.author
+        author: comment.author.trim() === '' ? 'Annonymous' : comment.author
       }))
     }
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="form-todo form-group">
+    <p>
+        <input placeholder="Author" type="text" name="author" class="form-control" v-model="author" />
+    </p>
+    <p>
+        <textarea placeholder="Comment" name="message"  class="form-control" v-model="message"></textarea>
+    </p>
+    <button @click="addComment" type="submit" class="btn btn-primary">Comment</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        name: '',
+        message: ''
+      }
+    },
+    methods: {
+      addComment() {
+        if (this.message.trim() === '') {
+          return;
+        }
+        this.$emit('add-todo', {
+          author: this.author,
+          message: this.message,
+        });
+        this.author = '';
+        this.message = '';
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
